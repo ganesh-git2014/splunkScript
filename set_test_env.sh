b080d5a3aae3d30b4220b43c52471c6f3a20ed1f
clin <clin@splunk.com>
2014-01-17 12:38:42 +0800

    Add functionality that i can choose which splunk instance i want to test agaist when i have multiple splunk on my laptop

--- set_test_env.sh
+++ set_test_env.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
-# export SPLUNK_HOME="$1"
 P4ROOT="/Users/clin/Documents/p4depot/splunk"
-BRANCH="$2"
+BRANCH="$1"
 
-if [ current == "$2" ]; then
+if [ current == "$1" ]; then
 	cd "$P4ROOT/current/new_test"
 else
-    cd "$P4ROOT/branches/$2/new_test"
+    cd "$P4ROOT/branches/$1/new_test"
 fi
 
-source setTestEnv "$1"
+
+splunks=(`findsplunk`)
+total=${#splunks[@]}
+if [ 1 == $total ]; then
+	source setTestEnv ${splunks[0]}
+else
+	echo "Which splunk do you want to run test?"
+	for (( i=0; i<=$(( $total -1 )); i++ ))
+	do
+		echo "$i) ${splunks[$i]}"
+	done
+	read index
+	source setTestEnv "${splunks[$index]}"
+fi
+
 cd "tests/web/webdriver"
